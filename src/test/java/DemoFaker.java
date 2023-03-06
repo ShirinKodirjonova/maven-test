f01208d761f30e186bb013c6f6f858d14f5833e3
Shirin <kadyrzanovasirina9@gmail.com>
2023-03-06 11:59:22 -0600

    first commit

---
+++ src/test/java/DemoFaker.java
@@ -0,0 +1,12 @@
+import com.github.javafaker.Faker;
+
+public class DemoFaker {
+
+    public static void main(String[]args){
+
+        Faker  faker = new Faker();
+        System.out.println(faker.address().firstName());
+
+        System.out.println(faker.chuckNorris().fact() );
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+
+public class test {
+
+    public static void main(String[]args){
+        System.out.println("Testing");
+
+
+
+    }
+}
